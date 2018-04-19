25ccf046ef663aa7e7165c27c2b3a9f7e36a9790
tokii <tokiinyan@gmail.com>
2018-04-20 01:10:59 +0800

    a

--- Mail-Spam-Filtering-master/Mail-Spam-Filtering-master/lingspam_filter.py
+++ Mail-Spam-Filtering-master/Mail-Spam-Filtering-master/lingspam_filter.py
@@ -10,9 +10,11 @@ import numpy as np
 from collections import Counter
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
-def make_Dictionary(train_dir):
+def make_dictionary(train_dir):
     emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
     all_words = []       
     for mail in emails:    
@@ -50,13 +52,49 @@ def extract_features(mail_dir):
                   features_matrix[docID,wordID] = words.count(word)
         docID = docID + 1     
     return features_matrix
-    
+
+def get_true_positive_sample(test_labels,result):
+    index = -1;
+    for i in range(len(test_labels)):
+        if test_labels[i] == result[i]:
+            if test_labels[i] == 1.0:
+                index = i
+    return index
+
+def get_true_negative_sample(test_labels,result):
+    index = -1;
+    for i in range(len(test_labels)):
+        if test_labels[i] == result[i]:
+            if test_labels[i] == 0.0:
+                index = i
+    return index
+
+def get_false_positive_sample(test_labels,result):
+    index = -1;
+    for i in range(len(test_labels)):
+        if test_labels[i] != result[i]:
+            if test_labels[i] == 0.0:
+                index = i
+    return index
+
+def get_false_negative_sample(test_labels,result):
+    index = -1;
+    for i in range(len(test_labels)):
+        if test_labels[i] != result[i]:
+            if test_labels[i] == 1.0:
+                index = i
+    return index
+
 # Create a dictionary of words with its frequency
 
-train_dir = 'C:\Users\lenovo\Mail-Spam-Filtering-master\Mail-Spam-Filtering-master\lingspam_public\lemm_stop\train-mails'
-dictionary = make_Dictionary(train_dir)
+print "Making Dictionary"
+
+train_dir = r"C:\Users\Sai\PycharmProjects\SpamIdentifier\Mail-Spam-Filtering-master\train-mails"
+dictionary = make_dictionary(train_dir)
 
 # Prepare feature vectors per training mail and its labels
+
+print "Preparing Training Vectors"
 
 train_labels = np.zeros(702)
 train_labels[351:701] = 1
@@ -64,23 +102,58 @@ train_matrix = extract_features(train_dir)
 
 # Training SVM and Naive bayes classifier and its variants
 
-model1 = LinearSVC()
+print "Training Classifier"
+
 model2 = MultinomialNB()
 
-model1.fit(train_matrix,train_labels)
 model2.fit(train_matrix,train_labels)
 
 # Test the unseen mails for Spam
 
-test_dir = 'C:\Users\lenovo\Mail-Spam-Filtering-master\Mail-Spam-Filtering-master\lingspam_public\lemm_stop\test-mails'
+print "Testing Dataset"
+
+test_dir = r"C:\Users\Sai\PycharmProjects\SpamIdentifier\Mail-Spam-Filtering-master\test-mails"
 test_matrix = extract_features(test_dir)
 test_labels = np.zeros(260)
 test_labels[130:260] = 1
 
-result1 = model1.predict(test_matrix)
 result2 = model2.predict(test_matrix)
+result_percent = accuracy_score(test_labels,result2)*100
 
-print confusion_matrix(test_labels,result1)
+print "Test Complete, Results:"
+
+print "Accuracy: " + str(result_percent)
 print confusion_matrix(test_labels,result2)
 
+print "Expected Labels"
+print test_labels
+print "Actual Labels"
+print result2
 
+print "True Positive"
+index = get_true_positive_sample(test_labels,result2)
+print "Email # " + str(index+1)
+print "Expected: " + str(test_labels[index]) + " Actual: " + str(result2[index])
+files = [os.path.join(test_dir,fi) for fi in os.listdir(test_dir)]
+print files[index]
+
+print "False Positive"
+index = get_false_positive_sample(test_labels,result2)
+print "Email # " + str(index+1)
+print "Expected: " + str(test_labels[index]) + " Actual: " + str(result2[index])
+files = [os.path.join(test_dir,fi) for fi in os.listdir(test_dir)]
+print files[index]
+
+print "True Negative"
+index = get_true_negative_sample(test_labels,result2)
+print "Email # " + str(index+1)
+print "Expected: " + str(test_labels[index]) + " Actual: " + str(result2[index])
+files = [os.path.join(test_dir,fi) for fi in os.listdir(test_dir)]
+print files[index]
+
+print "False Negative"
+index = get_false_negative_sample(test_labels,result2)
+print "Email # " + str(index+1)
+print "Expected: " + str(test_labels[index]) + " Actual: " + str(result2[index])
+files = [os.path.join(test_dir,fi) for fi in os.listdir(test_dir)]
+print files[index]
